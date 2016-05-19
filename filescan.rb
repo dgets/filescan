26129015938e323d1fd4ab05bd1d75b9e4689a28
Damon Getsman <damo.gets@gmail.com>
2016-05-19 06:50:29 -0700

    Surface debugging pass complete

--- filescan.rb
+++ filescan.rb
@@ -5,17 +5,18 @@
 
 require 'optparse'
 
-VERBOSE = true
+VERBOSE = false
 
 #at some point the archive types should be separated into a config file from
 #this script (JSON)
 
-archivers = { "{tgz|tar.gz}$" => "/bin/tar ztf" }
+#$archivers = Hash.new
+$archivers = { "{tgz|tar.gz}$" => "/bin/tar ztf" }
 
 #so apparently both .length and .size work here
 if (ARGV.length != 2)
     print "filescan.rb usage:\n\tfilescan.rb [target] [location] where [target]"
-    print " is the string that you're\n\tsearching for, in the [location].\n"
+    print " is the string that\n\tyou're searching for, in the [location].\n"
     exit 1
 else
     puts "looking for: #{ARGV[0]} in #{ARGV[1]}"
@@ -23,29 +24,62 @@ end
 
 TARGET = ARGV[0]
 LOCATION = ARGV[1]
+$success = false
+
+#check_dir(LOCATION)
 
 def check_dir(dirspec)
+    ouah = String.new
+
     Dir.foreach(dirspec) { |entry|
-	if (VERBOSE)
-	    puts "Looking at: #{dirspec}"
+	if ((entry == ".") or (entry == ".."))
+	    if (VERBOSE)
+		puts "Skipping: #{dirspec}"
+	    end
+	    next
 	end
 
-	archivers.each { |spec,command|
+        if (VERBOSE)
+            puts "Looking at: #{dirspec}"
+        end
+
+	#directory?
+	if (File.directory?(entry))
+	    check_dir(entry)
+	    next
+	end
+
+	#match in the present entry?
+	if (/#{TARGET}/ =~ entry)
+	    puts "#{entry}"
+	    $success = true
+	    next
+	end
+
+	#is it an archive that we support?
+	$archivers.each { |spec,command|
 	    if (/#{spec}/ =~ entry)
 		ouah = `#{command} #{spec}`
 	    end
 	    if (VERBOSE)
-		puts "Searching:\n#{ouah}\n"
+		puts "Searching:\n#{ouah}"
 	    end
 
-	    ouah.each { |line|
-		if (/#{ARGV[0]}/ =~ line)
-		    puts line
+	    ouah.split("\n") { |line|
+		if (/#{TARGET}/ =~ line)
+		    puts "#{line}"
+		    $success = true
 		end
 	    }
 	}
-
     }
+
+    if ($success == true)
+	return true
+    end
 end
 
+if (check_dir(LOCATION))
+    puts "Found #{TARGET} under #{LOCATION}\n"
+end
 
